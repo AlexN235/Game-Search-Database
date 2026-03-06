811a1fce48e32f58e977cbd546c2d8cb9838a6bf
Alex Nguyen <alex.nguyen235@gmail.com>
2026-03-05 16:13:07 -0800

    initial commit

---
+++ public/app.js
@@ -0,0 +1,11 @@
+// Javascript for frontend of webpage
+const searchBtn = document.querySelector('.submit-btn');
+const searchBar = document.getElementById('search-bar');
+
+searchBtn.addEventListener('click', GameSearch);
+
+function GameSearch() {
+    const textValue = searchBar.value;
+    console.log(window.location.href)
+    //window.location.href = window.location.href.toString().split("/")[1];
+}
